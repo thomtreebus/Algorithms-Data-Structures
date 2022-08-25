e0afafbb3142669a417bf9d95e489b276616d3d9
Thom Treebus <thomtreebus@gmail.com>
2022-08-25 16:17:11 +0200

    Implement bubble sort

---
+++ Algorithms/Sorting/BubbleSort.py
@@ -0,0 +1,20 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n - 1):
+        for j in range(n - i - 1):
+            if arr[j] > arr[j + 1]:
+                temp = arr[j+ 1]
+                arr[j + 1] = arr[j]
+                arr[j] = temp
+    return arr
+        
+            
+
+
+def main():
+    arr = [7, 3, 1, 8, 9, 4, 5, 6, 10, 2]
+    print(f"Original array: {arr}")
+    sorted_arr = bubble_sort(arr)
+    print(f"Sorted array: {sorted_arr}")
+
+main()
