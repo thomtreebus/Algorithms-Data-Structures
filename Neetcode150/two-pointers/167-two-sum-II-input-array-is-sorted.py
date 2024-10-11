ddea2731825a919c45203c9e923790eb67b513a4
Thom Treebus <thomtreebus@gmail.com>
2024-10-11 12:01:45 +0100

    add intution for 167

--- Neetcode150/two-pointers/167-two-sum-II-input-array-is-sorted.py
+++ Neetcode150/two-pointers/167-two-sum-II-input-array-is-sorted.py
@@ -1,4 +1,13 @@
 class Solution:
+    '''
+    intuition: we can use the fact that the input array is sorted to our advantage. 
+    By using two pointers at the ends of the array, we can simply check if the sum of the
+    numbers at those indices is equal to the target. If it's larger than the target,
+    then we know that we need to use a smaller value so we simply decrement the right pointer,
+    the same is true for when the sum is smaller than the target.
+    time: O(n)
+    space: O(1)
+    '''
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         left = 0
         right = len(numbers) - 1
