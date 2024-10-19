ae63ae4a95d297a328950b5fba0db33249b8208f
Thom Treebus <thomtreebus@gmail.com>
2024-10-19 20:08:21 +0100

    better solution for 150

--- Leetcode/Neetcode150/stack/150-evaluate-reverse-polish-notation.py
+++ Leetcode/Neetcode150/stack/150-evaluate-reverse-polish-notation.py
@@ -1,20 +1,20 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
+
         for token in tokens:
-            if token == "+":
+            if token in "+-/*":
                 a, b = stack.pop(), stack.pop()
-                stack.append(a + b)
-            elif token == "-":
-                a, b = stack.pop(), stack.pop()
-                stack.append(b - a)
-            elif token == "/":
-                a, b = stack.pop(), stack.pop()
-                stack.append(int(b/a))
-            elif token == "*":
-                a, b = stack.pop(), stack.pop()
-                stack.append(a * b)
+                match token:
+                    case "+":
+                        stack.append(a + b)
+                    case "-":
+                        stack.append(b - a)
+                    case "/":
+                        stack.append(int(b / a))
+                    case "*":
+                        stack.append(a * b)
             else:
                 stack.append(int(token))
         
-        return stack[0]
+        return stack[-1]
